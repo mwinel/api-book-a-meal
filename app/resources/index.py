3ddd6e03b2d316380a1ce1cac902e000b603683e
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-06 19:37:17 +0300

    Get Index response

--- app/resources/index.py
+++ app/resources/index.py
@@ -2,13 +2,8 @@ from flask import jsonify
 from flask_restful import Resource
 from .. import app
 
-
 class Index(Resource):
-    """
-    Manage responses to the index route.
-    URL: /api/v1/ or /api/v1/index
-    Request method: GET
-    """
+    # Call the method to Get the index response.
     def get(self):
         """ Return a welcome message. """
         return {"message": "Welcome to Book-a-meal."}
