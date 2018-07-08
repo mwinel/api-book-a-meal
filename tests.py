be149f04f68c6c8b71c6070691918796d96e380d
Nelson Murungi <nelsonmwirumubi@gmail.com>
2018-07-08 17:28:16 +0300

    Test menu update and delete

--- tests.py
+++ tests.py
@@ -115,5 +115,34 @@ class TestBase(TestCase):
 		self.assertIn(self.menu["name"], str(response.data))
 		self.assertIn('Finger lickin', str(response.data))
 
+	def test_update_a_menu(self):
+		""" Test API can update a menu. """
+		self.menu = {
+			"name": "KFC Special",
+			"description": "Finger lickin chicken"
+		}
+		response = self.app.post("/api/v1/menus", data = self.menu)
+		self.assertEqual(response.status_code, 201)
+		self.menu = {
+			"name": "KFC Special 3",
+			"description": "Just Eat Finger lickin chicken..."
+		}
+		response = self.app.put("/api/v1/menus/1", data = self.menu)
+		self.assertEqual(response.status_code, 201)
+
+	def test_delete_a_menu(self):
+		""" Test API can delete a menu. """
+		self.menu = {
+			"name": "KFC Special",
+			"description": "Finger lickin chicken"
+		}
+		response = self.app.post("/api/v1/menus", data = self.menu)
+		self.assertEqual(response.status_code, 201)
+		response = self.app.delete("/api/v1/menus/1", data = self.menu)
+		self.assertEqual(response.status_code, 200)
+		response = self.app.get("/api/v1/menus/1", data = self.menu)
+		output = json.loads(response.data)
+		self.assertEqual(output, {'message': 'Menu not found'})
+
 if __name__ == "__main__":
 	unittest.main()
